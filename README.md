d2dee47739b6fc3956411a00670dd5917a698d63
Brett Sandler <sandler.brett@gmail.com>
2014-10-05 18:47:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# .dotfiles!
+
+These are my dotfiles.
+
+## Usage
+
+script/bootstrap will install everything.  
+Anything with a `.zsh` extension will be sourced.  
+Anything with a `.symln_target` extension will be 
+symlinked as `$HOME/.{file}`.
+
+## Etc
+
+### These are not my dotfiles
+Much of this is from @holman's dotfiles and I also like to pinch a line or three from @peplin's dotfiles.  Some of the comments and base files are from zprezto.
+
+### TODO
+* symlink /etc/ zsh files
+* clean up whitespace
+* move zprezto stuff into dotfiles
+* get rid of zprezto
+* integrate mackup
+* link to the dotfile repos mentioned above
+* version control .vim folder
---
+++ osx/defaults.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+###############################################################################
+# Finder
+###############################################################################
+
+echo ""
+echo "Displaying full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo ""
+echo "Always open everything in Finder's list view"
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+echo ""
+echo "Show the ~/Library folder"
+chflags nohidden ~/Library
+
+echo ""
+echo "Set the Finder prefs for showing a few different volumes on the Desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+###############################################################################
+ 
+echo ""
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+ 
+echo ""
+echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+ 
+echo ""
+echo "Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+ 
+echo ""
+echo "Setting a blazingly fast keyboard repeat rate (ain't nobody got time fo special chars while coding!)"
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+
+
+
+echo ""
+echo "Disable the sudden motion sensor as it's not useful for SSDs"
+sudo pmset -a sms 0
+
+echo ""
+echo "Setting the icon size of Dock items to 48 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 48
