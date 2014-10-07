89f0ca0ed337485dac6d2b1ba875d416b58669d4
Brett Sandler <sandler.brett@gmail.com>
2014-10-07 09:44:23 -0400

    Tear more options out of prezto

---
+++ zsh/environment.zsh
@@ -0,0 +1,5 @@
+setopt COMBINING_CHARS    # Combine zero-length punctuation characters (accents)
+                          # with the base character.
+setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+
+
