1ea49167fa893f4ccf33054c409283548f9063fb
Brett Sandler <sandler.brett@gmail.com>
2014-10-22 22:14:28 +0200

    Update environment options

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,5 +1,10 @@
-setopt COMBINING_CHARS    # Combine zero-length punctuation characters (accents)
-                          # with the base character.
-setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
-
+setopt COMBINING_CHARS   # Combine zero-length punctuation characters (accents)
+                         # with the base character.
+# Jobs
+#
+setopt AUTO_RESUME        # Attempt to resume existing job before creating a new process.
+setopt NOTIFY             # Report status of background jobs immediately.
+unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
+unsetopt HUP              # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
