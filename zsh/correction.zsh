8831e7ca6bf1cd86aeb280950d9b82e9dc43ab5d
Brett Sandler <sandler.brett@gmail.com>
2014-10-26 19:47:55 +0100

    Add starter correction.zsh from prezto

---
+++ zsh/correction.zsh
@@ -0,0 +1,20 @@
+# Correct commands
+setopt CORRECT
+
+#### PUT CORRECTION STUFF HERE
+
+# Disable correction.
+alias ack='nocorrect ack'
+alias cd='nocorrect cd'
+alias cp='nocorrect cp'
+alias ebuild='nocorrect ebuild'
+alias gcc='nocorrect gcc'
+alias gist='nocorrect gist'
+alias grep='nocorrect grep'
+alias heroku='nocorrect heroku'
+alias ln='nocorrect ln'
+alias man='nocorrect man'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias rm='nocorrect rm'
