51432de72c7f78437b4614e2617321e794fe41d7
Brett Sandler <sandler.brett@gmail.com>
2014-11-14 12:43:21 -0500

    Add git completion to fpath

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,7 +1,9 @@
 # Use git completion from Homebrew
 completion='/usr/local/bin/share/zsh/site-functions/_git'
 
+# If we have it, add it to $fpath
 if test -f $completion
 then
+    $fpath=($completion $fpath)
     source $completion
 fi
