93d679c7f30481792d0238d67f03b3024ee69685
Brett Sandler <sandler.brett@gmail.com>
2015-01-26 20:32:06 -0500

    Alias vi and vim to macvim

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,6 +1,7 @@
 alias d='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
-alias vi="vim"
+alias vi="mvim -v"
+alias vim="mvim -v"
 
 alias ls='gls --color=auto --group-directories-first -F'
