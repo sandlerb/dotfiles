562eaeb1d8150bf7541b76701b18af762021c847
Brett Sandler <sandler.brett@gmail.com>
2016-02-14 18:02:54 -0500

    Only use mvim and gls on OS X

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,8 +1,14 @@
 alias d='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
-alias vi="mvim -v"
-alias vim="mvim -v"
+if [ "$(uname -s)" == "Darwin" ]
+then
+    alias vi="mvim -v"
+    alias vim="mvim -v"
 
-alias ls='gls --color=auto --group-directories-first -F'
-alias date='gdate'
+    alias ls='gls --color=auto --group-directories-first -F'
+    alias date='gdate'
+else
+    alias vi="vim"
+    alias ls='ls --color==auto --group-directories-first -F'
+fi
