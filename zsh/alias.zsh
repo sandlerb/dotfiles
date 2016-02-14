df074b6705e64872f8ecda36cf81bb38638173cf
Brett Sandler <sandler.brett@gmail.com>
2016-02-14 18:53:20 -0500

    Correct OS checking

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,14 +1,13 @@
 alias d='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
-if [ "$(uname -s)" == "Darwin" ]
-then
+if [[ "$(uname -s)" == "Darwin" ]]; then
     alias vi="mvim -v"
     alias vim="mvim -v"
 
     alias ls='gls --color=auto --group-directories-first -F'
     alias date='gdate'
-else
+elif [[ "$(uname -s)" == "Linux" ]]; then
     alias vi="vim"
     alias ls='ls --color==auto --group-directories-first -F'
 fi
