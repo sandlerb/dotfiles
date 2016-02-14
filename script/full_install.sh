1928881e602e80496a007bf06fa67a1dab1bb71c
Brett Sandler <sandler.brett@gmail.com>
2016-02-14 17:46:25 -0500

    Allow homebrew or apt-get

--- script/full_install.sh
+++ script/full_install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-if [[ "$(uname -s)" == "Darwin" ]]
-then
-    source ../homebrew/install.sh
-fi
+PS3='Which application manager would you like to us:'
+options=("homebrew" "apt-get" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "Proceeding with homebrew"
+            source ../homebrew/install.sh
+            break
+            ;;
+        "Option 2")
+            echo "Proceeding with apt-get"
+            source ./ubuntu_install.sh
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
 source bootstrap
