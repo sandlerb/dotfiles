18e8839760707d933cefff3685a3cf3bb3228782
Brett Sandler <bsandler@uberatc.com>
2016-05-25 17:51:09 -0400

    Update lots of things

--- terminal/alias.zsh
+++ terminal/alias.zsh
@@ -1,2 +1,7 @@
-alias ltty='ls /dev/tty.*'
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    alias ltty='ls /dev/tty.*'
+elif [[ "$(uname -s)" == "Linux" ]]; then
+    alias ltty='ls /dev/tty*'
+fi
+
 alias hag='history | tail -200 | ag'
