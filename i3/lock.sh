b7786effc433c42e39502e6d5bfbad4cd34bd249
Brett Sandler <sandler.brett@gmail.com>
2017-07-06 21:17:16 -0400

    Add i3lock, update pkglist, fix blinking in less

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Screenshot the current screen
 scrot /tmp/lock_scrot.png
-convert /tmp/lock_scrot.png -blur 3x3 -colorspace Gray /tmp/lock_scrot_conv.png
+
+# Pixelate it and make it gray
+mogrify -scale 10% -scale 1000% /tmp/lock_scrot.png
+convert /tmp/lock_scrot.png -colorspace Gray /tmp/lock_scrot_conv.png
+
+# Lock it up
 i3lock -i /tmp/lock_scrot_conv.png
+
+# Turn the screen off after a delay
+sleep 60; pgrep i3lock && xset dpms force off
