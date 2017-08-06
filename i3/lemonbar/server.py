c86d25240d2ba026ecc992cb0f224ef0467a7350
Brett Sandler <sandler.brett@gmail.com>
2017-08-06 19:00:44 -0400

    Update lemonbar usage to use new API

--- i3/lemonbar/server.py
+++ i3/lemonbar/server.py
@@ -2,7 +2,6 @@
 
 from lemonpy.ipc import LemonpyServer
 from lemonpy.lemonbar import Lemonbar
-from lemonpy.manager import LemonpyManager
 
 import time
 
@@ -14,22 +13,13 @@ def run():
     # TODO -B base03 -F base0
     bottom_bar = Lemonbar(permanent=True, bottom=True, font='Hack:size=10')
 
-    # TODO I don't think there's any value in having the manager as a public class,
-    # possibly any value at all
-    lm = LemonpyManager()
-    lm.register(BAR_NAME, bottom_bar)
-
-    ls = LemonpyServer(SOCKET_NAME, lm)
-    ls.run()
-
-    while True:
-        try:
-            # TODO need a stopping mechanism
-            bottom_bar.update(get_content())
-            time.sleep(REFRESH_PERIOD_S)
-        except KeyboardInterrupt:
-            ls.close()
-            break
+    with LemonpyServer(SOCKET_NAME, bars={BAR_NAME: bottom_bar}):
+        while True:
+            try:
+                bottom_bar.update(get_content())
+                time.sleep(REFRESH_PERIOD_S)
+            except KeyboardInterrupt:
+                break
 
 
 if __name__ == '__main__':
