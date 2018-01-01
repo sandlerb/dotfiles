277683af00afc183262a032b1189f45521a7bece
Brett Sandler <sandler.brett@gmail.com>
2018-01-01 16:06:31 -0500

    More i3/polybar tweaks

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,6 +2,6 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
 polybar --config=$HOME/.dotfiles/polybar/config.ini status &
